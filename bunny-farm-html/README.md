579cd16ab9475a5c638b313b452d1e19c3bcbb4e
Alexander Pritchard <>
2025-01-20 20:45:27 -0500

    Getting bunnies saving, loading, and released.
    Adding readme as well. Adding logs when adding bunnies.

---
+++ bunny-farm-html/README.md
@@ -0,0 +1,15 @@
+# Caileys Bunny Farm
+
+INSTRUCTIONS:
+
+```bash
+python -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install flask
+python app.py
+```
+
+If any of these commands fail, paste your console output to ChatGPT and let it know the following:
+
+"I am trying to setup my basic Flask server in Python inside of a Virtual Environment specific to the project. The project has a venv folder with the virtual environment. Instead of running, the application is producing the errors I pasted above. Please give me step-by-step instructions for identifying and debugging the issue. I am on Windows."
--- bunny-farm-html/app.py
+++ bunny-farm-html/app.py
@@ -20,7 +20,9 @@ def load_bunnies():
 
 
 def save_bunnies(bunnies):
+    # Save the list of bunnies to the JSON file
     with open(DATA_FILE, "w") as f:
+        # Indent the JSON data for easier reading
         json.dump(bunnies, f, indent=4)
 
 
@@ -29,45 +31,69 @@ def index():
     bunnies = load_bunnies()
     return render_template("index.html", bunnies=bunnies)
 
+
 # Map image filenames to CSS classes
 image_classes = {
-    'bear.png': 'bear',
-	'buffalo.png': 'buffalo',
-	'chick.png': 'chick',
-	'chicken.png': 'chicken',
-	'cow.png': 'cow',
-	'crocodile.png': 'crocodile',
-	'dog.png': 'dog',
-	'duck.png': 'duck',
-	'elephant.png': 'elephant',
-	'frog.png': 'frog',
-	'giraffe.png': 'giraffe',
-	'goat.png': 'goat',
-	'gorilla.png': 'gorilla',
-	'hippo.png': 'hippo',
-	'horse.png': 'horse',
-	'monkey.png': 'monkey',
-	'moose.png': 'moose',
-	'narwhal.png': 'narwhal',
-	'owl.png': 'owl',
-	'panda.png': 'panda',
-	'parrot.png': 'parrot',
-	'penguin.png': 'penguin',
-	'pig.png': 'pig',
-	'rabbit.png': 'rabbit',
-	'rhino.png': 'rhino',
-	'sloth.png': 'sloth',
-	'snake.png': 'snake',
-	'walrus.png': 'walrus',
-	'whale.png': 'whale',
-	'zebra.png': 'zebra'
+    "bear.png": "bear",
+    "buffalo.png": "buffalo",
+    "chick.png": "chick",
+    "chicken.png": "chicken",
+    "cow.png": "cow",
+    "crocodile.png": "crocodile",
+    "dog.png": "dog",
+    "duck.png": "duck",
+    "elephant.png": "elephant",
+    "frog.png": "frog",
+    "giraffe.png": "giraffe",
+    "goat.png": "goat",
+    "gorilla.png": "gorilla",
+    "hippo.png": "hippo",
+    "horse.png": "horse",
+    "monkey.png": "monkey",
+    "moose.png": "moose",
+    "narwhal.png": "narwhal",
+    "owl.png": "owl",
+    "panda.png": "panda",
+    "parrot.png": "parrot",
+    "penguin.png": "penguin",
+    "pig.png": "pig",
+    "rabbit.png": "rabbit",
+    "rhino.png": "rhino",
+    "sloth.png": "sloth",
+    "snake.png": "snake",
+    "walrus.png": "walrus",
+    "whale.png": "whale",
+    "zebra.png": "zebra",
 }
+
 
 @app.route("/add_bunny", methods=["POST"])
 def add_bunny():
     bunnies = load_bunnies()
+
+    # Print the list of bunnies to the console
+    print("")
+    print("=========================")
+    print("BUNNY LIST: ", bunnies)
+    print("=========================")
+    print("")
+
+    # Print the form data to the console
+    print("")
+    print("=========================")
+    print("FORM DATA: ", request.form)
+    print("=========================")
+    print("")
+
     name = request.form["name"]
     image = request.form["image"]
+
+    # Log that we're creating the bunny
+    print("")
+    print("=========================")
+    print("Creating bunny with name: ", name)
+    print("=========================")
+    print("")
     new_bunny = {
         "id": len(bunnies) + 1,
         "name": name,
@@ -82,7 +108,29 @@ def add_bunny():
             "Age": 0,
         },
     }
+
+    # Log the bunny before adding it to the list
+    print("")
+    print("=========================")
+    print("NEW BUNNY: ", new_bunny)
+    print("=========================")
+    print("")
     bunnies.append(new_bunny)
+
+    # Save the new list of bunnies
+    save_bunnies(bunnies)
+    return redirect(url_for("index"))
+
+
+@app.route("/release_bunny/<int:bunny_id>", methods=["POST"])
+def release_bunny(bunny_id):
+    # Remove bunny from the list, and re-save the list of bunny
+    bunnies = load_bunnies()
+    for bunny in bunnies:
+        if bunny["id"] == bunny_id:
+            # Remove that bunny from the list
+            bunnies.remove(bunny)
+            break
     save_bunnies(bunnies)
     return redirect(url_for("index"))
 
